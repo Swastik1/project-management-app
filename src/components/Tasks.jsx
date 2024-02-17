b268e50e15ffa6017863599fff5d2f4bf2ee3d9a
Swastik <swastikpanda32@gmail.com>
2024-02-17 19:40:28 +0530

    ...fixed style

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,7 +6,7 @@ const Tasks = () => {
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask />
-      <p className="text-stone-800 mb-4">
+      <p className="text-stone-800 my-4">
         This project does not have any tasks yet
       </p>
       <ul></ul>
